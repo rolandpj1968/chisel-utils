9bff6fe56634a70e8a1cdccc8dd2a2f67d06eae9
Roland Paterson-Jones <rolandpj@gmail.com>
2025-01-06 19:46:00 +0200

    start decoding... squash xtra fields into 1 - they should be mutually exclusive

--- src/main/scala/org/clanpj/chisel/mam/alu/decode/Decoder.scala
+++ src/main/scala/org/clanpj/chisel/mam/alu/decode/Decoder.scala
@@ -12,6 +12,9 @@ object AluOpcode extends ChiselEnum {
   val AluOpcSub2 =     Value(0x05.U)
   val AluOpcAdd4 =     Value(0x06.U)
   val AluOpcSub4 =     Value(0x07.U)
+
+
+
   val AluOpcFF   =     Value(0xff.U) // Enforce 8-bit width
 }
 
@@ -23,6 +26,10 @@ object AluSrc1 extends ChiselEnum {
   val Src1None, Src1Nos, Src1Lit = Value
 }
 
+object Src1LitX extends ChiselEnum {
+  val LitX0, LitX1, LitX2, LitX4 = Value
+}
+
 object AluOp extends ChiselEnum {
   val OpNone, OpAdd, OpShift, OpBits, OpExt, OpInv = Value
 }
@@ -31,29 +38,43 @@ class Decoder extends Module {
   import AluOpcode._
   import AluSrc0._
   import AluSrc1._
+  import Src1LitX._
   import AluOp._
 
   val io = IO(new Bundle {
     val opc = Input(AluOpcode())
     val src0 = Output(AluSrc0())
-    val src0X = Output(UInt(2.W))
-    val src0M = Output(Bool())
+    val src0N = Output(Bool())
     val src1 = Output(AluSrc1())
-    val src1X = Output(UInt(2.W))
-    val src1M = Output(Bool())
+    val src1N = Output(Bool())
     val op = Output(AluOp())
-    val subop = Output(UInt(3.W))
     val wr = Output(Bool())
-    val dItos = Output(UInt(2.W))
+    val dITos = Output(UInt(2.W))
+    val xtra = Output(UInt(3.W)) // Src1LitX, 
   })
 
-  val src0 = Src0None; val src0X = 0.U(2.W); val src0M = false.B;
-  val src1 = Src1None; val src1X = 0.U(2.W); val src1M = false.B;
-  val op = OpInv; val subop = 0.U(3.W)
+  val src0 = Src0None; val src0N = false.B;
+  val src1 = Src1None; val src1N = false.B;
+  val op = OpInv
   val wr = false.B
   val dITos = 0.U(2.W)
+  val xtra = 0.U(3.W)
 
   switch (io.opc) {
-    is (AluOpcNop) { op := OpNone; }
+    is (AluOpcNop)  { op := OpNone; }
+    is (AluOpcDrop) { op := OpNone; dITos := 3.U; }
+    is (AluOpcAdd1) { op := OpAdd; src0 := Src0Tos; src1 := Src1Lit;                  xtra := LitX1.asUInt; wr := true.B; }
+    is (AluOpcSub1) { op := OpAdd; src0 := Src0Tos; src1 := Src1Lit; src1N := true.B; xtra := LitX1.asUInt; wr := true.B; }
+    is (AluOpcAdd2) { op := OpAdd; src0 := Src0Tos; src1 := Src1Lit;                  xtra := LitX2.asUInt; wr := true.B; }
+    is (AluOpcSub2) { op := OpAdd; src0 := Src0Tos; src1 := Src1Lit; src1N := true.B; xtra := LitX2.asUInt; wr := true.B; }
+    is (AluOpcAdd4) { op := OpAdd; src0 := Src0Tos; src1 := Src1Lit;                  xtra := LitX4.asUInt; wr := true.B; }
+    is (AluOpcSub4) { op := OpAdd; src0 := Src0Tos; src1 := Src1Lit; src1N := true.B; xtra := LitX4.asUInt; wr := true.B; }
   }
+
+  io.src0 := src0; io.src0N := src0N;
+  io.src1 := src1; io.src1N := src1N;
+  io.op := op
+  io.wr := wr
+  io.dITos := dITos
+  io.xtra := xtra
 }
