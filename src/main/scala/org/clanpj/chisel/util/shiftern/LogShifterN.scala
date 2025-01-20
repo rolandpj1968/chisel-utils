696fa43cb9a8e0f8be1f1d998136f38d37ea3d0b
Roland Paterson-Jones <rolandpj@gmail.com>
2025-01-20 14:25:18 +0200

    Attempt at a O(logN) shifter

---
+++ src/main/scala/org/clanpj/chisel/util/shiftern/LogShifterN.scala
@@ -0,0 +1,39 @@
+/*
+ * O(logN) n-bit shifter
+ */
+
+package org.clanpj.chisel.util.shiftern
+
+import chisel3._
+import chisel3.util._
+
+class LogShifterN(n: Int) extends ShifterN(n) {
+  val nbits = log2Ceil(n)
+  //val shift = io.y(nbits-1, 0)
+  val over = io.y(n-1, nbits).orR
+  val sign = io.x(n-1)
+  val fill = Mux(io.arith, sign, false.B)
+
+  val leftn = (0 to nbits).map(_ => Wire(UInt(n.W)))
+  leftn(nbits) := io.x
+  for(bn <- nbits-1 to 0 by -1) {
+    val vbn = Cat(leftn(bn+1)(n-(1 << bn), 0), Fill(1 << bn, 0.U))
+    leftn(bn) := Mux(io.y(bn), vbn, leftn(bn+1))
+  }
+
+  val rightn = (0 to nbits).map(_ => Wire(UInt(n.W)))
+  rightn(nbits) := io.x
+  for(bn <- nbits-1 to 0 by -1) {
+    val vbn = Cat(Fill(1 << bn, sign), rightn(bn+1)(n, (1 << bn)-1))
+    rightn(bn) := Mux(io.y(bn), vbn, rightn(bn+1))
+  }
+
+  when (over) {
+    io.v := Mux(io.left || !io.arith, 0.U, Fill(n, sign))
+  } otherwise {
+    io.v := Mux(io.left, leftn(0), rightn(0))
+  }
+  //printf("log-shiftern: x %d y %d left %d arith %d -> v %d\n", io.x, io.y, io.left, io.arith, io.v)
+
+  def desc = "simple" + n
+}
